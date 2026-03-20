8dbf414a7a9e9923f7745ec9053e4d2b18319a27
Raghava Averineni <143755942+Raghavaaverineni11@users.noreply.github.com>
2026-03-20 01:26:24 -0500

    Initial Commit

---
+++ patient-service/src/main/java/com/pm/patient_service/mapper/PatientMapper.java
@@ -0,0 +1,21 @@
+package com.pm.patient_service.mapper;
+
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.model.Patient;
+
+public class PatientMapper {
+
+    public static PatientResponseDTO toDTO(Patient patient){
+
+        PatientResponseDTO patientDTO = new PatientResponseDTO();
+        patientDTO.setAddress(patient.getAddress());
+        patientDTO.setEmail(patient.getEmail());
+        patientDTO.setName(patient.getName());
+        patientDTO.setId(patient.getId().toString());
+        patientDTO.setDateOfBirth(patientDTO.getDateOfBirth());
+
+        return patientDTO ;
+
+
+    }
+}
