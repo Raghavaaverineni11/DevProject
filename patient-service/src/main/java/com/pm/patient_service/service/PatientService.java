95d60952f36035dc3656bf938c7a313456868fb1
Raghava Averineni <143755942+Raghavaaverineni11@users.noreply.github.com>
2026-05-31 05:19:48 -0500

    kafka producer Added in Patient Service,protbuf creted for message event for kafka

--- patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -6,6 +6,7 @@ import com.pm.patient_service.dto.PatientResponseDTO;
 import com.pm.patient_service.exception.EmailAlreadyExistsException;
 import com.pm.patient_service.exception.PatientNotFoundException;
 import com.pm.patient_service.grpc.BillingServiceGrpcClient;
+import com.pm.patient_service.kafka.KafkaProducer;
 import com.pm.patient_service.mapper.PatientMapper;
 import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
@@ -22,10 +23,12 @@ public class PatientService {
 
     private final BillingServiceGrpcClient billingServiceGrpcClient;
 
+    private final KafkaProducer kafkaProducer;
 
-    public PatientService(PatientRepository patientRepository,BillingServiceGrpcClient billingServiceGrpcClient){
+    public PatientService(PatientRepository patientRepository,BillingServiceGrpcClient billingServiceGrpcClient,KafkaProducer kafkaProducer){
         this.patientRepository=patientRepository;
         this.billingServiceGrpcClient=billingServiceGrpcClient;
+        this.kafkaProducer=kafkaProducer;
     }
 
     public List<PatientResponseDTO> getPatients(){
@@ -41,6 +44,7 @@ public class PatientService {
                Patient newPatient=patientRepository.save(PatientMapper.toModel(patient));
 
                billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),newPatient.getName(),newPatient.getEmail());
+               kafkaProducer.sendEvent(newPatient);
 
                return PatientMapper.toDTO(newPatient);
     }
